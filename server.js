36c51eba2582f97977e7556f1fb62925613f92ca
artemklimenko96 <artemklimenko96@gmail.com>
2017-03-15 01:33:20 +0200

    Adapted "package.json and sercer.js" to Heroku deployment

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 var express = require('express');
 //CREATE OUR app
 var app = express();
+const PORT = proess.env.PORT || 3000;
+app.use(function(req, res, next){
+  if(req.headers['x-forwarded-proto']==='http'){
+    next();
+  }else{
+    res.redirect('http://' + req.hostname + req.url);
+  }
+});
 app.use(express.static('public'));
-app.listen(3000, function(){
+app.listen(PORT, function(){
 
-console.log("express server is up on port 3000");  
+console.log("express server is up on port " + PORT);
 });
