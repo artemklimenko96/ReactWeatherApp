d0456c3545a8f0f54233b732114af5cf6d6b9da4
artemklimenko96 <artemklimenko96@gmail.com>
2017-03-15 14:01:28 +0200

    Changed About message

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,7 +1,10 @@
 var React = require('react');
 var About = (props) =>{
       return(
-      <h3>About component</h3>
+        <div>
+      <h3>About</h3>
+      <p>Welcome to About page</p>
+        </div>
     )
   };
 module.exports = About;
