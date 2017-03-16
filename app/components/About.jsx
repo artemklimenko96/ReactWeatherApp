383cc9fe67b044746d49903fd1608b3b35059bc2
Artem Klimenko <artemklimenko96@gmail.com>
2017-03-16 16:58:09 +0200

    Styled about page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -2,8 +2,17 @@ var React = require('react');
 var About = (props) =>{
       return(
         <div>
-      <h3>About</h3>
-      <p>Welcome to About page</p>
+      <h1 className = "text-center">About</h1>
+      <p>This is my first application created using React js</p>
+      <p>Tools used:</p>
+      <ul>
+        <li>
+          <a href="https://facebook.github.io/react">React</a> -The js framework used
+        </li>
+        <li>
+          <a href="https://openweathermap.org">Open weather map</a> - API used to fetch the data
+        </li>
+      </ul>
         </div>
     )
   };
