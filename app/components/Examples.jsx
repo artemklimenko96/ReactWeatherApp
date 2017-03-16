c969e8949eaae638f6ef67348400ccdd55eb9e91
Artem Klimenko <artemklimenko96@gmail.com>
2017-03-16 16:47:17 +0200

    Styled examples page with foundation

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,9 +1,18 @@
 var React = require('react');
+var {Link}=require('react-router');
 var Examples = (props) =>{
       return(
         <div>
-      <h3>Examples</h3>
-      <p>Welcome to examples page!</p>
+      <h1 className = "text-center">Examples</h1>
+      <p>Here are a few example locations to try out:</p>
+      <ol>
+        <li>
+        <Link to ='/?location=Oulu'>Oulu</Link>
+        </li>
+        <li>
+        <Link to ='/?location=Frankfurt'>Frankfurt</Link>
+        </li>
+      </ol>
         </div>
     )
   };
